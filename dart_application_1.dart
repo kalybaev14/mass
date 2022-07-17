dd867f5af3b796d06e54006e220ff7488e37a2a2
kalybaev14 <kalybaevajnabek@gmail.com>
2022-07-17 19:45:25 +0300

    add

---
+++ dart_application_1.dart
@@ -0,0 +1,46 @@
+import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+
+void main(List<String> arguments) {
+//List mas=  [1, 7, 12, 3, 56, 2, 87, 34, 54];
+//print(mas.first);
+//print(mas[4]);
+//print(mas.last);
+
+
+/*List<int> a = [3, 12, 43, 1, 25, 6, 5, 7];
+List<int> b =  [55, 11, 23, 56, 78, 1, 9];
+a.addAll(b);
+print(a);*/
+
+/*List z = ['a','d','F','l','u','t','t','e','R','y','3','b','h','j'];
+print(z.sublist(2,9));*/
+
+/*List x= [1, 2, 3, 4, 5, 6, 7];
+print(x.contains(3));
+print(x.length);*/
+
+/*List<dynamic> c = [601, 123, 2, "dart", 45, 95, "dart24", 1];
+var q=c.contains('dart');
+var w=c.contains(951);
+bool r=true;
+r=q&w;
+print(r);*/
+
+
+/*List<dynamic> t=['post', 1, 0, 'flutter'];
+String myFlutter='Flutter';
+print(t.contains(myFlutter.toLowerCase()));*/
+
+/*List<dynamic> y=['I', 'Started', "Learn", "Flutter", "Since", "April"];
+String myFlutter='';
+myFlutter.addAll(y);
+print(myFlutter);*/
+
+
+List u=[1, 9, 3, 195, 202, 2, 5, 7, 9, 10, 3, 15, 0, 11];
+u.sort();
+print(u);
+
+
+
+}
